a17df3e814c9ff22672debc44fe654312df2d5e2
ordepMendes <pedrohh0012@hotmail.com>
2024-04-18 19:59:02 -0300

    feat: :sparkles: First commit - Iniciação do projeto, instalação de CDN do bootstrap e grid provisorio

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+let num = ref(0);
+</script>
+
+
+<template>
+  <nav class="navbar bg-dark navbar-dark">
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand"><img src="./assets/images/logo-header.png" width="25px"/> Pokédex </router-link>
+    </div>
+  </nav>
+  <div class="main">
+    <router-view></router-view>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <main class="mt-5">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-sm-12 col-md-6">
+          <div class="card mb-3" style="max-width: 540px">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="../assets/images/teste.png" class="img-fluid rounded-start" alt="..." />
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Charmander</h5>
+                  <p class="card-text">
+                    teste
+                  </p>
+                  <p class="card-text">
+                    <small class="text-body-secondary"
+                      >teste</small
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">Column</div>
+      </div>
+    </div>
+  </main>
+</template>
